216b6160c6b5f6a989dfc9b43fe24e4e32a848c8
WBujna <wojciech.bujna@zs6sobieski.edu.pl>
2021-03-15 01:15:49 +0100

    added add, remove methods

--- src/App.js
+++ src/App.js
@@ -3,30 +3,39 @@ import React, { useState, useRef } from "react";
 const App = () => {
   const [list, setList] = useState([
     { id: 0, value: "dupa-0", isChecked: false },
-    { id: 1, value: "dupa-1", isChecked: false },
-    { id: 2, value: "dupa-2", isChecked: false },
-    { id: 3, value: "dupa-3", isChecked: false },
-    { id: 4, value: "dupa-4", isChecked: false },
   ]);
+  const [postId, setPostId] = useState(1);
   const input = useRef(null);
+
+  const addPost = () => {
+    setList([
+      ...list,
+      { id: postId, value: input.current.value, isChecked: false },
+    ]);
+    setPostId(postId + 1);
+  };
+  const removePost = (id) => {
+    const modifiedPosts = list.filter((item) => {
+      if (id === item.id) return null;
+      return item;
+    });
+    setList(modifiedPosts);
+  };
+
   return (
     <div>
       <ul>
         {list.map((item) => {
-          return <li>{item.value}</li>;
+          return (
+            <li key={item.id}>
+              <span>{item.value}</span>
+              <button onClick={() => removePost(item.id)}>Remove</button>
+            </li>
+          );
         })}
       </ul>
       <input type="text" ref={input} />
-      <button
-        onClick={() =>
-          setList([
-            ...list,
-            { id: undefined, value: input.current.value, isChecked: false },
-          ])
-        }
-      >
-        Zmien tekst
-      </button>
+      <button onClick={addPost}>Zmien tekst</button>
     </div>
   );
 };
